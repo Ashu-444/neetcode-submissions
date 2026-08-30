2ece7d8d15a494e3c33e561d264a6fb1b5f56852
Ashu-444 <ashutoshsrivastav444@gmail.com>
2026-08-30 10:51:39 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        helper(nums , 0 , ans);
+        return ans;
+    }
+    public void helper(int[] nums , int ind , List<List<Integer>> ans){
+        List<Integer> ds = new ArrayList<>();
+        if(ind == nums.length){
+            for(int i = 0; i < nums.length; i++){
+                ds.add(nums[i]);
+            }
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+
+
+        for(int i = ind; i < nums.length; i++){
+            swap(i , ind , nums);
+            helper(nums , ind + 1 , ans);
+            swap(i , ind , nums);
+        }
+    }
+    
+    public void swap(int i , int j , int[] nums){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
