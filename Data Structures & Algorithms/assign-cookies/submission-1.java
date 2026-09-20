ece4040c316435bea52e4b7849787f236a0363d5
Ashu-444 <ashutoshsrivastav444@gmail.com>
2026-09-20 12:23:16 +0530

    Add: assign-cookies - submission-1

---
+++ Data Structures & Algorithms/assign-cookies/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int n = g.length;
+        int m = s.length;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int left = 0 , right = 0;
+
+        while(left < n && right < m){
+            if(g[left] <= s[right]){
+                left++;
+                
+            }
+            right++;
+        }
+        return left;
+    }
+}
